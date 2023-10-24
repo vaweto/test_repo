d07121d99b7488fd6f5e6c3b7000ff6a6ebe448f
vaweto <vagelisbisbikis@gmail.com>
2023-10-24 21:10:37 +0300

    only on php 8.2 and one small test fix

--- tests/WeightTest.php
+++ tests/WeightTest.php
@@ -1,5 +1,8 @@
 <?php
 
-it('can test', function () {
-    expect(true)->toBeTrue();
+it('can convert kilograms to lbs', function () {
+    $lbs = \Vaweto\TestRepo\Weight::fromKilograms(10)
+        ->toLbs();
+
+    expect(22.04623)->toBe($lbs);
 });
